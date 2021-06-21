9055dd0c1fb0e55a58ff158827bd1721f509edf5
Hendra Manudinata <hendralize@gmail.com>
2021-06-21 10:44:10 +0700

    bruh

--- dump.sh
+++ dump.sh
@@ -6,7 +6,7 @@ git config --global user.email "hendralize@gmail.com"
 git clone https://github.com/hendralizeStuff/phoenix_firmware_dumper fwdump && cd fwdump
 
 # Set up required variables
-echo ${GH_TOKEN} > .github_token
+echo ${GH_PAT} > .github_token
 echo "hendralizeFwDump" > .github_orgname
 echo ${BOT_TOKEN} > .tg_token
 echo "1195724888" > .tg_chat
